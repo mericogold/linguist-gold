63166dc6faa10d14ce1c7907742d084196f3f1f9
mericogold <mericogold@merico.gonze.com>
2021-02-10 08:28:44 -0800

    Third commit

--- main.c
+++ main.c
@@ -1,3 +1,32 @@
+void init() {
+    block_init();
+    state.window = &window;
+    renderer_init(&state.renderer);
+    world_init(&state.world);
+    ui_init(&state.ui);
+    mouse_set_grabbed(true);
+
+    struct Entity player = ecs_new(&state.world.ecs);
+    ecs_add(player, C_POSITION);
+    ecs_add(player, C_PHYSICS, ((struct PhysicsComponent) {
+        .size = {
+            (vec3s) {{ 0, 0, 0 }},
+            (vec3s) {{ 0.2f, 1.6f, 0.2f }}
+        },
+        .flags = {
+            .gravity = true,
+            .collide = true
+        }
+    }));
+    ecs_add(player, C_MOVEMENT, ((struct MovementComponent) {
+        .speed = 1.0f,
+        .jump_height = 1.0f
+    }));
+    ecs_add(player, C_CAMERA, ((struct CameraComponent) {
+        .offset = (vec3s) {{ 0.0f, 0.8f, 0.0f }}
+    }));
+}
+
 void destroy() {
     renderer_destroy(&state.renderer);
     world_destroy(&state.world);
